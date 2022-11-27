96dedd42519e49eb229fc5bfb6d85dbce5045b83
David Fontenot <davidmfontenot@gmail.com>
2022-11-26 23:42:58 -0800

    putting together more of the ssr react app

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,5 @@
 export * as Oak from 'https://deno.land/x/oak@v11.1.0/mod.ts'
+export * as Mustache from 'https://deno.land/x/mustache@v0.3.0/mod.ts'
 import React from 'https://esm.sh/react@18'
 export { React }
 export * as ReactDOMServer from 'https://esm.sh/react-dom@18/server'
---
+++ src/ssr/App.tsx
@@ -0,0 +1,3 @@
+import { React } from '../deps.ts'
+
+export const App = () => <h1>React SSR test</h1>
