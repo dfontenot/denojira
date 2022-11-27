a7dd796ba80d3df22592e09897657e851adff60d
David Fontenot <davidmfontenot@gmail.com>
2022-11-26 23:11:29 -0800

    setting up for react SSR

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,5 @@
 export * as Oak from 'https://deno.land/x/oak@v11.1.0/mod.ts'
-export * as React from 'https://esm.sh/react@18'
+import React from 'https://esm.sh/react@18'
+export { React }
+export * as ReactDOMServer from 'https://esm.sh/react-dom@18/server'
+
