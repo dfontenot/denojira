a5b29bf77c934d88113cc0c2b653e16aa0e57c60
David Fontenot <davidmfontenot@gmail.com>
2022-11-28 20:17:46 -0800

    try setting the target on the frontend deps to es2020

--- src/deps-backend.ts
+++ src/deps-backend.ts
@@ -6,3 +6,7 @@ export * as Mustache from 'https://deno.land/x/mustache@v0.3.0/mod.ts'
 export * as Oak from 'https://deno.land/x/oak@v11.1.0/mod.ts'
 export * as Path from 'https://deno.land/std/path/mod.ts'
 
+// considered backend only for renderToString
+import React from 'https://esm.sh/react@18'
+export { React }
+export * as ReactDOMServer from 'https://esm.sh/react-dom@18/server'
--- src/deps-frontend.ts
+++ src/deps-frontend.ts
@@ -1,10 +1,10 @@
-import React from 'https://esm.sh/react@18'
+import React from 'https://esm.sh/react@18?target=es2020'
 export { React }
-export * as ReactDOMServer from 'https://esm.sh/react-dom@18/server'
-export * as ReactDOM from 'https://esm.sh/react-dom@18'
-export * as ReactRedux from 'https://esm.sh/react-redux@8.0'
+export * as ReactDOMServer from 'https://esm.sh/react-dom@18/server?target=es2020'
+export * as ReactDOM from 'https://esm.sh/react-dom@18?target=es2020'
+export * as ReactRedux from 'https://esm.sh/react-redux@8.0?target=es2020'
 // TODO: fix, how to import optional addon @reduxjs/toolkit/query/react ?
-export * as ReduxToolkit from 'https://esm.sh/@reduxjs/toolkit@1.9'
+export * as ReduxToolkit from 'https://esm.sh/@reduxjs/toolkit@1.9?target=es2020'
 
 // TODO: not necessary? appears to bring in @reduxjs/toolkit/dist/query instead of @reduxjs/toolkit/query/react
 // https://stackoverflow.com/a/68569190/854854
