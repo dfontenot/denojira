31c8874dfef42e817aad25b764d25bc14e376454
David Fontenot <davidmfontenot@gmail.com>
2023-01-09 22:22:11 -0800

    updated to denodb 1.2

--- src/deps-backend.ts
+++ src/deps-backend.ts
@@ -1,4 +1,4 @@
-export * as DenoDB from 'https://deno.land/x/denodb@v1.1.0/mod.ts'
+export * as DenoDB from 'https://deno.land/x/denodb@v1.2.0/mod.ts'
 export * as DenoEmit from 'https://deno.land/x/emit@0.11.0/mod.ts'
 export * as ESBuild from 'https://deno.land/x/esbuild@v0.15.16/mod.js'
 export * as ESBuildDenoLoader from 'https://deno.land/x/esbuild_deno_loader@0.6.0/mod.ts'
