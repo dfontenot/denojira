744ff27c1a7358a3da47699c02e63403599fb1b6
David Fontenot <davidmfontenot@gmail.com>
2023-01-10 23:39:20 -0800

    Revert "bringing in chakra ui"
    This reverts commit cb2971ac572fdb96c1a77f4290cb6f667cad77b1.
    
    chakra UI won't work with SSR

--- src/deps-frontend.ts
+++ src/deps-frontend.ts
@@ -8,13 +8,6 @@ export * as ReactRedux from 'https://esm.sh/react-redux@8.0?target=es2020'
 // TODO: fix, how to import optional addon @reduxjs/toolkit/query/react ?
 export * as ReduxToolkit from 'https://esm.sh/@reduxjs/toolkit@1.9?target=es2020'
 
-export * as FramerMotion from 'https://esm.sh/framer-motion@8.4?target=es2020'
-export * as EmotionStyled from 'https://esm.sh/@emotion/styled@11.10?target=es2020'
-export * as EmotionReact from 'https://esm.sh/@emotion/react@11.10?target=es2020'
-export * as ChakraUI from 'https://esm.sh/@chakra-ui/react@2.4?target=es2020'
-export * as chakraTheme from 'https://esm.sh/@chakra-ui/theme@2.2?target=es2020'
-
-
 // TODO: not necessary? appears to bring in @reduxjs/toolkit/dist/query instead of @reduxjs/toolkit/query/react
 // https://stackoverflow.com/a/68569190/854854
 //export * as RTK from 'https://esm.sh/@reduxjs/toolkit@1.9/query/react'
